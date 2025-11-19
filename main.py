051c7522b5c4cd12559916c4771f0e2d11a98544
manonlortal-sys <manonlortal@gmail.com>
2025-11-19 09:21:34 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ from flask import Flask
 import discord
 from discord.ext import commands
 
-from storage import create_db, upsert_guild_config, upsert_team
-
 app = Flask(__name__)
 
 @app.get("/")
@@ -37,10 +35,6 @@ async def setup_hook():
 
     for ext in [
         "cogs.alerts",
-        "cogs.reactions",
-        "cogs.leaderboard",
-        "cogs.attackers",
-        "cogs.attaque",
         "cogs.pvp",
         "cogs.rota",
     ]:
@@ -65,30 +59,4 @@ async def on_ready():
 
 if __name__ == "__main__":
     print("⚡ Booting…")
-
-    create_db()
-
-    upsert_guild_config(
-        guild_id=1139550147190214727,
-        alert_channel_id=1139550892471889971,
-        leaderboard_channel_id=1421866004270682113,
-        snapshot_channel_id=1421866144679329984,
-        role_g1_id=1419320456263237663,
-        role_g2_id=1421860260377006295,
-        role_g3_id=1421859079755927682,
-        role_g4_id=1419320615999111359,
-        role_test_id=1421867268421320844,
-        admin_role_id=1139578015676895342
-    )
-
-    upsert_team(1139550147190214727, 1, "Wanted", 1419320456263237663, "WANTED 1", 1)
-    upsert_team(1139550147190214727, 2, "Wanted 2", 1421860260377006295, "WANTED 2", 2)
-    upsert_team(1139550147190214727, 3, "Snowflake", 1421859079755927682, "SNOWFLAKE", 3)
-    upsert_team(1139550147190214727, 4, "Secteur K", 1419320615999111359, "SECTEUR K", 4)
-    upsert_team(1139550147190214727, 5, "Rixe", 1421927584802934915, "RIXE", 5)
-    upsert_team(1139550147190214727, 6, "HAGRATIME", 1421927858967810110, "HAGRATIME", 6)
-    upsert_team(1139550147190214727, 7, "HagraPaLtime", 1421927953188524144, "HAGRAPALTIME", 7)
-    upsert_team(1139550147190214727, 8, "Prisme", 1421953218719518961, "PRISME", 8)
-    upsert_team(1139550147190214727, 9, "Ruthless", 1437841408856948776, "RUTHLESS", 9)
-
     bot.run(DISCORD_TOKEN)
